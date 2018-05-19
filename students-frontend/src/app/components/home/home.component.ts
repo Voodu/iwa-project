2bdc9944e8e8cc1defaa8effdbf7e411797f1e19
Piotr Ładoński <piolado@gmail.com>
2018-05-19 18:51:32 +0200

    Implemented HomeComponent

--- students-frontend/src/app/components/home/home.component.ts
+++ students-frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Student, Course, Grade } from '../../models';
+import { DataService } from '../../services';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    title = 'Students App';
+    students = new Array<Student>();
 
-  ngOnInit() {
-  }
+    constructor(private dataService: DataService) {
+    }
 
+    ngOnInit(): void {
+        this.dataService.getStudents().subscribe(data => this.students = data, error => this.students = this.dataService.getMockStudents());
+    }
+
+    update(student: Student) {
+        this.dataService.updateStudent(student).subscribe(data => console.log(`Updated student id...`));
+    }
 }
