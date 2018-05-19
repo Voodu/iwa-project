9174aa0c9be2e74f36d24fe4bb4c70ceb99d7dac
Piotr Ładoński <piolado@gmail.com>
2018-05-19 18:51:18 +0200

    Added HttpModule

--- students-frontend/src/app/app.module.ts
+++ students-frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
@@ -39,6 +40,7 @@ const routes: Routes = [
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         FormsModule,
         NgbModule.forRoot(),
         RouterModule.forRoot(routes),
