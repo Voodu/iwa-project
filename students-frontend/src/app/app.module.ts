7e60d9257f81024ce11de367957980448cdb4b22
Piotr Ładoński <piolado@gmail.com>
2018-05-19 14:53:31 +0200

    Fixes for Bootstrap and FA5 configuration

--- students-frontend/src/app/app.module.ts
+++ students-frontend/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { GradeComponent } from './components/grade/grade.component';
+
+// Font Awesome icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+library.add(faMinus);
+library.add(faPlus);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        GradeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgbModule.forRoot(),
+        FontAwesomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
