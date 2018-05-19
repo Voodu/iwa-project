f165e3e8ad0e3f00dba03ec6c0a9e19183b9b10d
Piotr Ładoński <piolado@gmail.com>
2018-05-19 15:49:26 +0200

    Added index.ts for components

---
+++ students-frontend/src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { AboutComponent } from './about/about.component';
+export { GradeComponent } from './grade/grade.component';
+export { GradesViewComponent } from './grades-view/grades-view.component';
+export { HomeComponent } from './home/home.component';
