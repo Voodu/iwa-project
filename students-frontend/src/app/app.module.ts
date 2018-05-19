fc88469a29575c94dd6738c60011c3f594538463
Piotr Ładoński <piolado@gmail.com>
2018-05-19 15:41:54 +0200

    Added basic navbar & router

--- students-frontend/src/app/app.module.ts
+++ students-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
@@ -11,19 +12,28 @@ import { GradeComponent } from './components/grade/grade.component';
 // Font Awesome icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { AboutComponent } from './components/about/about.component';
 library.add(faMinus);
 library.add(faPlus);
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        GradeComponent
+        GradeComponent,
+        AboutComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         NgbModule.forRoot(),
+        RouterModule.forRoot(routes),
         FontAwesomeModule
     ],
     providers: [],
