497fd7e10ffd8e7fb458fcd64f2dacc7ed976e56
Piotr Ładoński <piolado@gmail.com>
2018-06-09 18:27:20 +0200

    Refactored database structure

--- students-backend/src/main/java/com/github/voodu/studentrest/controller/StudentController.java
+++ students-backend/src/main/java/com/github/voodu/studentrest/controller/StudentController.java
@@ -1,25 +1,31 @@
 package com.github.voodu.studentrest.controller;
 
+import com.github.voodu.studentrest.model.Course;
 import com.github.voodu.studentrest.model.Student;
-import com.github.voodu.studentrest.model.request.AddStudentRequest;
+import com.github.voodu.studentrest.repository.CourseRepository;
 import com.github.voodu.studentrest.repository.StudentRepository;
+import com.github.voodu.studentrest.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/public/students")
 public class StudentController {
     private StudentRepository studentRepository;
+    private CourseRepository courseRepository;
 
     @Autowired
-    public StudentController(StudentRepository studentRepository)
+    public StudentController(StudentRepository studentRepository, CourseRepository courseRepository)
     {
         this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -31,34 +37,43 @@ public class StudentController {
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public Student findStudent(@PathVariable long id)
     {
-        return studentRepository.getOne(id);
+//        return studentRepository.getOne(id);
+        return new Student();
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public Student addStudent(@RequestBody AddStudentRequest addStudentRequest)
+    public void addStudent(@RequestBody Student student)
     {
-        Student student = new Student();
-        student.setName(addStudentRequest.getName());
-        student.setSurname(addStudentRequest.getSurname());
-        student.setFaculty(addStudentRequest.getFaculty());
-        student.setCourses(addStudentRequest.getCourses());
-        return studentRepository.save(student);
+        Course course = courseRepository.findAll().get(0);
+        Set<Course> courses = new HashSet<>();
+        courses.add(course);
+        student.setCourses(courses);
+
+
+
+        return;
+//        Student student = new Student();
+//        student.setName(addStudentRequest.getName());
+//        student.setSurname(addStudentRequest.getSurname());
+//        student.setFaculty(addStudentRequest.getFaculty());
+//        student.setCourses(addStudentRequest.getCourses());
+//        return studentRepository.save(student);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    public Student putStudent(@RequestBody AddStudentRequest putStudentRequest)
+    public void putStudent(@RequestBody Student student)
     {
-        Student student = new Student();
-        student.setId(putStudentRequest.getId());
-        student.setName(putStudentRequest.getName());
-        student.setSurname(putStudentRequest.getSurname());
-        student.setFaculty(putStudentRequest.getFaculty());
-        student.setCourses(putStudentRequest.getCourses());
-        return studentRepository.save(student);
+//        Student student = new Student();
+//        student.setId(putStudentRequest.getId());
+//        student.setName(putStudentRequest.getName());
+//        student.setSurname(putStudentRequest.getSurname());
+//        student.setFaculty(putStudentRequest.getFaculty());
+//        student.setCourses(putStudentRequest.getCourses());
+//        return studentRepository.save(student);
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Student> deleteContact (@PathVariable("id") long id) {
+    public ResponseEntity<Student> deleteStudent (@PathVariable("id") long id) {
 
         try
         {
