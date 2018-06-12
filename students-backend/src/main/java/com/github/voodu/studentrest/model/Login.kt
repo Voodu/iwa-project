d1ea2e4995e0bc866e90936db08553fbe2a3894e
wefhy <wefhy007@gmail.com>
2018-06-12 08:48:52 +0200

    added explanation

--- students-backend/src/main/java/com/github/voodu/studentrest/model/Login.kt
+++ students-backend/src/main/java/com/github/voodu/studentrest/model/Login.kt
@@ -1,3 +1,3 @@
 package com.github.voodu.studentrest.model
 
-class Login (val username: String, val password: String)
+class Login (val username: String, val password: String) //this class is just for login request.
